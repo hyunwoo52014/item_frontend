a10fb76512376dd98c2cf09715fe20961dcdae9f
JHyun12864 <hyeon12864@naver.com>
2025-09-23 13:49:18 +0900

    2025 09 23 오후 1:48 현지영 / first commit

---
+++ src/pages/requests/Approvals.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+import '../../assets/css/admin/common.css';
+
+const Approvals = () => {
+
+
+    return (
+        <div id="container">
+			<ul>
+				<li className="contents">
+					<div className="content">
+                        <p class="Location">
+                            <a href="../dashboard/dashboard.do" className="btn_set home">메인으로</a>
+                            <span className="btn_nav bold">신청/반납</span>
+                            <span className="btn_nav bold">사용신청</span>
+                            <a href="/requests/apply" className="btn_set refresh"></a>
+                        </p>
+						<p className="conTitle">
+							<span>결제</span>
+							<span className="fr">
+								<select id="searchKey" name="searchKey" style={{width:"100px"}}>
+									<option value="">전체</option>
+									<option value="itProduct">IT 장비</option>
+									<option value="name">이름</option>
+								</select>
+								<input type="text" style={{width:"300px", height:"25px"}} id="searchword" name="searchword"/>
+								<a href="#" className="btnType blue" id="btnSearchUser" name="btn"><span>검 색</span></a>
+							</span>
+						</p>
+
+						<div id="divProductList">
+							<table className="col">
+								<thead>
+									<tr>
+										<th>번호</th>
+										<th>이름</th>
+										<th>대여 장비</th>
+										<th>신청날짜</th>
+										<th>상태</th>
+									</tr>
+								</thead>
+								<tbody id="approvalsList">
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tbody>
+							</table>
+						</div>
+						
+						<div className="paging_area">아마 이 부분에 reactpagination을 넣으면 될 듯</div>
+					</div>
+
+				</li>
+			</ul>
+		</div>
+    );
+};//approvals
+
+export default Approvals;
