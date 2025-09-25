90f81f4b157b9724b1fd83b5bdbbce27767f6ec8
KYS <dydtjr12300@gmail.com>
2025-09-25 10:32:21 +0900

    Add asset pages

---
+++ src/pages/asset/InboundModal.jsx
@@ -0,0 +1,13 @@
+// InboundModal.jsx - 최소 버전
+import React from 'react';
+
+const InboundModal = ({ modalState, closeModal }) => {
+    return (
+        <div>
+            {/* 임시 모달 내용 */}
+            <p>모달 작업중...</p>
+        </div>
+    );
+};
+
+export default InboundModal;
